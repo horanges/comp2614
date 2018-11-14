6f1fb2b2c43179eaeac84ab93db25d7fc8f88f3d
horanges <45024179+horanges@users.noreply.github.com>
2018-11-13 19:40:15 -0800

    Add files via upload

---
+++ COMP2614Assign01/COMP2614Assign02/COMP2614Assign02/ConsolePrinter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP2614Assign02
+{
+    class ConsolePrinter
+    {
+        const int LINE_LENGTH = 30;
+
+        public static void PrintContacts(Contact[] contacts)
+        {
+            Console.WriteLine("\nContacts");
+            Console.WriteLine(new string('-', LINE_LENGTH));
+
+            foreach(Contact contact in contacts)
+            {
+                Console.WriteLine(contact.ToString());
+                Console.WriteLine(contact.Address);
+                Console.WriteLine("{0} {1}  {2}\n\n", contact.City, contact.Province, contact.PostalCode);
+            }
+        }
+    }
+}
---
+++ COMP2614Assign01/COMP2614Assign02/COMP2614Assign02/Program.cs
@@ -0,0 +1,83 @@
+﻿// COMP2614 Assign02
+// Creates 3 instances of a Contact class, adds them to an array and then outputs content to console
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP2614Assign02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName;
+            string lastName;
+            string address;
+            string city;
+            string province;
+            string postalCode;
+
+            const int LINE_LENGTH = 30;
+            const int CONTACTS_LENGTH = 3;
+
+            // Sets console title
+            Console.Title = "COMP2614 - Assignment 2 - A00947294";
+
+            Console.WriteLine("Contact Information");
+            Console.WriteLine(new string('-', LINE_LENGTH));
+
+            // Prompts and collects contact information from user
+            Console.Write("Firstname:   ");
+            firstName = Console.ReadLine();
+
+            Console.Write("Lastname:    ");
+            lastName = Console.ReadLine();
+
+            Console.Write("Address:     ");
+            address = Console.ReadLine();
+
+            Console.Write("City:        ");
+            city = Console.ReadLine();
+
+            Console.Write("Province:    ");
+            province = Console.ReadLine();
+
+            Console.Write("Postal Code: ");
+            postalCode = Console.ReadLine();
+
+            // Adds contacts to Contact array
+            Contact[] contacts = new Contact[CONTACTS_LENGTH];
+
+            // Empty Contact object and then populated via properties
+            Contact contact1 = new Contact();
+            contact1.FirstName = firstName;
+            contact1.LastName = lastName;
+            contact1.Address = address;
+            contact1.City = city;
+            contact1.Province = province;
+            contact1.PostalCode = postalCode;
+
+            contacts[0] = contact1;
+
+            // Parameterized constructor
+            contacts[1] = new Contact(firstName, lastName, address, city, province, postalCode);
+
+            // Object Initializer syntax
+            contacts[2] = new Contact()
+            {
+                FirstName = firstName,
+                LastName = lastName,
+                Address = address,
+                City = city,
+                Province = province,
+                PostalCode = postalCode
+            };
+
+            // Prints contacts
+            ConsolePrinter.PrintContacts(contacts);
+        }
+    }
+}
